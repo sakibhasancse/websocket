c12abc61eb775400225dfd12e0a800d47b30e6f0
SakibHasanCse <sakibhasan9641@gmail.com>
2021-08-19 20:23:22 +0600

    added

--- server/server.js
+++ server/server.js
@@ -1,4 +1,21 @@
-const Websocket = require('ws');
-const wss = new WebSocket.Server({ port: 7071 });
-const clients = new map.Map();
-const exoress = require('exoress');
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const path = require('path');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+dotenv.config();
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+})
+
+setInterval(() => {
+    io.emit('image', 'some data');
+}, 1000)
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+    console.log('listening on port' + PORT);
+})
